b964a60538a1052a3bb5607dd74f7d447bd4041a
sasa <mostafayounis630@gmail.com>
2025-12-05 10:53:46 +0200

    add blog page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { Blog } from './features/blog/blog';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'works', redirectTo: '', pathMatch: 'full' },
-  { path: 'blog', redirectTo: '', pathMatch: 'full' },
+  { path: 'blog', component:Blog},
   { path: 'contact', redirectTo: '', pathMatch: 'full' }
 ];
